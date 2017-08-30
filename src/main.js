fb4c815632788b769cd05ce192bb7e583ec4ae8d
wudong <wudong@hey900.com>
2017-08-30 20:57:33 +0800

    头部

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+
+import './common/stylus/index.styl'
 
 Vue.config.productionTip = false
+
+window.axios = axios
+Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
 new Vue({
