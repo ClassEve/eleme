f8f51d778b3cfef4810759bdcd2ad82ea5aa698f
wudong <wudong@hey900.com>
2017-09-01 18:29:05 +0800

    左侧菜单

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,13 +1,87 @@
 <template>
-    <div>
-      我是goods
+    <div class="goods">
+      <div class="menu-wrapper">
+        <ul>
+          <li v-for="item in goods" class="menu-item">
+            <span class="text border-1px">
+              <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{ item.name }}
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div class="foods-wrapper"></div>
     </div>
 </template>
 
 <script>
-  export default {}
+  const ERR_OK = 0
+
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+        goods: {},
+        classMap: ['decrease', 'discount', 'guarantee', 'invoice', 'special']
+      }
+    },
+    created () {
+      this.$http.get('/api/goods').then(res => {
+        res = res.data
+        if (res.errno === ERR_OK) {
+          this.goods = res.data
+        }
+      })
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  @import "../../common/stylus/mixin.styl"
+  .goods
+    display flex
+    position absolute
+    width 100%
+    top 176px
+    bottom: 46px
+    overflow hidden
+    .menu-wrapper
+      flex 0 0 80px
+      width 80px
+      background: #f3f5f7
+      .menu-item
+        display table
+        padding: 0 12px
+        height: 54px
+        width 56px
+        line-height: 14px
+        .text
+          display table-cell
+          width 56px
+          vertical-align middle
+          font-size 12px
+          border-1px(rgba(7,17,27,0.1))
+        .icon
+          display inline-block
+          vertical-align:top
+          width 12px
+          height 12px
+          margin-right 2px
+          background-size 12px  12px
+          background-repeat:no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
+    .foods-wrapper
+      flex 1
 </style>
