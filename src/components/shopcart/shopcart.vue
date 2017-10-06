a029046239e7b8c5e954d9a3d61fba930f8431e3
ClassName <wdd667@outlook.com>
2017-10-07 02:42:54 +0800

    goodsdone

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,39 +1,89 @@
 <template>
-  <div class="shopcart">
-    <div class="content">
-      <div class="content-left">
-        <div class="logo-wrapper">
-          <div class="logo" :class="{'highLight': totalCount > 0}">
-            <i class="icon-shopping_cart" :class="{'highLight': totalCount > 0}"></i>
+  <div>
+    <div class="shopcart">
+      <div class="content" @click="toggleList">
+        <div class="content-left">
+          <div class="logo-wrapper">
+            <div class="logo" :class="{'highLight': totalCount > 0}">
+              <i class="icon-shopping_cart" :class="{'highLight': totalCount > 0}"></i>
+            </div>
+            <div class="num" v-show="totalCount > 0">{{ totalCount }}</div>
           </div>
-          <div class="num" v-show="totalCount > 0">{{ totalCount }}</div>
+          <div class="price" :class="{'highLight': totalPrice > 0}">￥{{ totalPrice }}</div>
+          <div class="desc">另需配送费￥{{ deliveryPrice}}元</div>
         </div>
-        <div class="price" :class="{'highLight': totalPrice > 0}">￥{{ totalPrice }}</div>
-        <div class="desc">另需配送费￥{{ deliveryPrice}}元</div>
+        <div class="content-right" @click.stop.prevent="pay">
+          <div class="pay" :class="payClass">
+            {{ payDesc }}
+          </div>
+        </div>
       </div>
-      <div class="content-right">
-        <div class="pay" :class="payClass">
-          {{ payDesc }}
+      <div class="shopcart-list" v-show="listShow">
+        <div class="list-header">
+          <h1 class="title">购物车</h1>
+          <span class="empty" @click="empty">清空</span>
+        </div>
+        <div class="list-content" ref="listContent">
+          <ul>
+            <li class="food" v-for="food in selectFoods">
+              <span class="name">{{food.name}}</span>
+              <div class="price">
+                <span>￥{{food.price * food.count}}</span>
+              </div>
+              <div class="cartcontrol-wrapper">
+                <CartControl :food="food"></CartControl>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
+    <div class="list-mask" @click="hideList" v-show="listShow"></div>
   </div>
 </template>
 
 <script>
+  import CartControl from '../cartcontrol/cartcontrol.vue'
+  import BScroll from 'better-scroll'
+
   export default {
     props: {
       selectFoods: {
-        type: Array,
-        default () {
-          return []
-        }
+        type: Array
       },
       deliveryPrice: {
         type: Number
       },
       minPrice: {
         type: Number
+      }
+    },
+    data () {
+      return {
+        fold: true
+      }
+    },
+    methods: {
+      pay () {
+        if (this.totalPrice < this.minPrice) {
+          return
+        } else {
+          alert(`支付${this.totalPrice}元`)
+        }
+      },
+      hideList () {
+        this.fold = true
+      },
+      empty () {
+        this.selectFoods.forEach((food) => {
+          food.count = 0
+        })
+      },
+      toggleList () {
+        if (!this.totalCount) {
+          return
+        }
+        this.fold = !this.fold
       }
     },
     computed: {
@@ -67,12 +117,36 @@
         } else {
           return 'enough'
         }
+      },
+      listShow () {
+        if (!this.totalCount) {
+          this.fold = true
+          return false
+        }
+        let show = !this.fold
+        if (show) {
+          this.$nextTick(() => {
+            if (!this.scroll) {
+              this.scroll = new BScroll(this.$refs.listContent, {
+                click: true
+              })
+            } else {
+              this.scroll.refresh()
+            }
+          })
+        }
+        return show
       }
+    },
+    components: {
+      CartControl
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
 .shopcart
   position fixed
   bottom 0
@@ -163,4 +237,59 @@
         &.enough
           background: #00b43c
           color #fff
+  .shopcart-list
+    position absolute
+    bottom:48px
+    left: 0
+    z-index -1
+    width 100%
+    .list-header
+      height 40px
+      line-height 40px
+      padding 0 18px
+      background: #f3f5f7
+      border-bottom 1px solid rgba(7,17,27,0.1)
+      .title
+        float left
+        font-size 14px
+        color rgb(7, 17, 27)
+      .empty
+        float right
+        font-size 12px
+        color rgb(0, 160, 221)
+    .list-content
+      padding 0 18px
+      max-height 217px
+      overflow:hidden
+      background: #fff
+      .food
+        position relative
+        padding 12px 0
+        box-sizing border-box
+        border-1px(rgba(7,17,27,0.1))
+        .name
+          line-height 24px
+          font-size 14px
+          color rgb(7,17,27)
+        .price
+          position absolute
+          right 90px
+          bottom 12px
+          line-height 24px
+          font-size 14px
+          font-weight 700
+          color red
+        .cartcontrol-wrapper
+          position absolute
+          right 0
+          bottom 6px
+.list-mask
+  position fixed
+  top 0
+  left 0
+  width 100%
+  height:100%
+  z-index 40
+  background: rgba(7, 17, 27, 0.6)
+  backdrop-filter: blur(10px)
 </style>
